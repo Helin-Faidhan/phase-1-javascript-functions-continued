841b7afcb48f488fc35c95035bb8d70fc9ac31d8
Helin Faidhan <helinkurd1997@gmail.com>
2022-10-17 20:14:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (message='roller-skate')  {
+return `This Saturday, I want to ${message}!`
+
+}
+
+function mondayWork (message='go to the office'){
+    return `This Monday, I will ${message}.`
+}
+
+function wrapAdjective (message='*'){
+    return function(name="special")
+    {
+        return `You are ${message}${name}${message}!`  
+    }
+}
